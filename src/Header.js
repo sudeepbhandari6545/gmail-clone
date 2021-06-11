8b0edc65e94896a02feb8d4703b3c24652f0260a
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-06-11 09:00:27 +0545

    linkedin

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,34 @@
 import React from 'react'
+//icons
+import { IconButton } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
+
 import './Header.css'
 
 function Header() {
-  return <div>this is header</div>
+  return (
+    <>
+      <div className="header">
+        <div className="header_left">
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+          <img
+            src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+            alt=""
+          />
+        </div>
+        <div className="header_middle">
+          <SearchIcon />
+          <input type="text" placeholder="search" />
+          <ArrowDropDownIcon className="dropdown_icon" />
+        </div>
+        <div className="header_right"></div>
+      </div>
+    </>
+  )
 }
 
 export default Header
