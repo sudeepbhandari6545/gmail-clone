6c4baf64bc0541fc18717debc53bdfcd577339d6
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-06-13 12:14:33 +0545

    gmailclone

--- src/SendMail.js
+++ src/SendMail.js
@@ -1,7 +1,61 @@
 import React from 'react'
 
+//icons
+import { Button } from '@material-ui/core'
+import CloseIcon from '@material-ui/icons/Close'
+
+import './Sendmail.css'
+import { useForm } from 'react-hook-form'
+
 function SendMail() {
-  return <div></div>
+  const { register, handleSubmit, watch, errors } = useForm()
+
+  const onSubmit = (formData) => {
+    console.log(formData)
+  }
+  return (
+    <>
+      <div className="sendMail">
+        <div className="sendMail_header">
+          <h3>New Message</h3>
+          <CloseIcon className="sendmail_close" />
+        </div>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            name="to"
+            placeholder="To"
+            type="email"
+            {...register('to', { required: 'Required' })}
+          />
+          <input
+            name="subject"
+            type="text"
+            placeholder="Subject"
+            {...register('subject', { required: 'Required' })}
+          />
+          <input
+            name="message"
+            placeholder="Message.."
+            type="text"
+            className="sendMail_messages"
+            {...register('message', { required: 'Required' })}
+          />
+
+          <div className="sendmail_option">
+            <Button
+              className="sendmail_send"
+              variant="contained"
+              color="primary"
+              type="submit"
+            >
+              Send
+            </Button>
+          </div>
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default SendMail
